c4d7f34ea701ce73de19c9c218c2999ec62d61ed
Ho Yin LEE <hoyin.lee@singtaonewscorp.com>
2019-11-15 16:20:16 +0800

    new ads type

--- application/models/Ads.php
+++ application/models/Ads.php
@@ -26,6 +26,10 @@ class Ads extends My_Model  {
 		// var_dump($data);
 		$ads_section = $this->config->item('ads_cat_list_pos');
 		foreach($data as $k => $ads){
+			if($ads['ads_type']==6){
+				$data[$k]['ads_image'] = $this->select_ads_imgs($ads['id']);
+			}
+			
 			$data[$k]['list_pos'] = strval($ads_section[$section][$ads['pos']-1]);
 		}
 		return  $this->ads_cast($data);
@@ -38,7 +42,7 @@ class Ads extends My_Model  {
 		
 		$return_data = array();
 		foreach ($data as $value) {
-			$return_data[] = array(
+			$tmp = array(
 				'id'			=>$value['id'],
 				'landing_type'	=>$value['landing_type'],
 				'landing_url'	=>($value['landing_type']==1)?$value['landing_url']:$this->config->item('base_url').'ads_view/'.$value['id']."?platform=".array_keys($this->platform_type,$this->platform)[0],
@@ -47,9 +51,26 @@ class Ads extends My_Model  {
 				'cover'			=>$value['id'],
 				'pos'			=>$value['pos'],
 				'list_pos'		=>$value['list_pos'],
-				'image'			=>$value['ads_image'],
+				
 				'layout'		=>$value['ads_type']
 			);
+			if($value['ads_type']==6){
+				foreach($value['ads_image'] as $k => $v){
+					if($k<3){
+						$tmp['image'][] = array(
+							'path'	=>	$v['src'],
+							"isCover" => ($v['pos']==0?1:0),
+						);
+					}
+				} 
+			}
+			else{
+				$tmp['image'][] = isset($value['ads_image'])?array(
+					'path'	=>	$value['ads_image'],
+					"isCover" => 1,
+				):array();
+			}
+			$return_data[] = $tmp;
 		}
 		return $return_data;
 
